dde34920d97549c1bf5a593bd01e92a0f7a06237
Asif0810 <asifaslam1703@gmail.com>
2022-12-26 23:06:50 +0600

    complete

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const uri = `mongodb+srv://${process.env.CAKE_DATABASE}:${process.env.CAKE_DB_PA
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
-    const cakesCollection = client.db('allcakes').collection('cakes')
+    const cakesCollection = client.db('allcakes').collection('cakes');
+    const reviewCollection = client.db('allcakes').collection('reviews');
     try {
         app.get('/cakes', async (req, res) => {
             const query = {};
@@ -35,6 +36,35 @@ const run = async () => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await cakesCollection.findOne(query);
+            res.send(result)
+        })
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
+        })
+        app.get('/reviewlist', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review)
+        })
+        app.get('/myreview', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query)
+            const review = await cursor.toArray();
+            res.send(review)
+
+        })
+        app.delete('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
             res.send(result)
         })
     }

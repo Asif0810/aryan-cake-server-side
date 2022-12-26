8b0fbb99bfa1416ff4c8f52ac155b1b268ba7ee8
Asif0810 <asifaslam1703@gmail.com>
2022-12-26 11:54:27 +0600

    deployed

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = 5000
-const require = ('cors');
+const cors = require('cors');
 require('dotenv').config();
+// for get data
 app.use(cors())
+// for post dat
+app.use(express.json())
+// CakeDB10
+// eKmtqzibCI8xFUdG
+
+
+
+const uri = `mongodb+srv://${process.env.CAKE_DATABASE}:${process.env.CAKE_DB_PASS}@cluster0.h9wahhk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    const cakesCollection = client.db('allcakes').collection('cakes')
+    try {
+        app.get('/cakes', async (req, res) => {
+            const query = {};
+            const cursor = cakesCollection.find(query);
+            const cakes = await cursor.limit(3).toArray();
+            res.send(cakes)
+        })
+        app.get('/allcakes', async (req, res) => {
+            const query = {};
+            const cursor = cakesCollection.find(query);
+            const cakes = await cursor.toArray();
+            res.send(cakes)
+        })
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await cakesCollection.findOne(query);
+            res.send(result)
+        })
+    }
+    catch {
+
+    }
+}
+run().catch(console.error())
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('!')
+    res.send('cake server is running!')
 })
 
 app.listen(port, () => {
